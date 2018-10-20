f9b5a3145a5f88f7e3ced6dbc3828a0d9cb5f12d
Tomasz Klim <github@tomaszklim.pl>
2018-10-20 14:29:46 +0200

    split into 2 new extensions, now it is just a proxy extension

--- setup.sh
+++ setup.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
 
-/opt/farm/scripts/setup/extension.sh sf-versioning
-/opt/farm/scripts/setup/extension.sh sf-php
-
-echo "setting up base directories and files"
-mkdir -p   /etc/local/.farm /var/cache/farm
-
-chmod 0700 /etc/local/.farm
-
-chmod 0710 /var/cache/farm
-chown root:www-data /var/cache/farm
-
-touch /etc/local/.farm/inspect.root
-
-if [ ! -f /etc/local/.farm/expand.json ]; then
-	echo -n "{}" >/etc/local/.farm/expand.json
-fi
-
-if ! grep -q /opt/farm/ext/farm-inspector/cron /etc/crontab; then
-	echo "48 6 * * *   root /opt/farm/ext/farm-inspector/cron/users.sh" >>/etc/crontab
-	echo "10 7 * * 1-6 root /opt/farm/ext/farm-inspector/cron/space.sh" >>/etc/crontab
-	echo "10 7 * * 7   root /opt/farm/ext/farm-inspector/cron/space.sh --force" >>/etc/crontab
-fi
-
-# transitional code
-if grep -q /opt/farm/ext/farm-inspector/cron/network.sh /etc/crontab; then
-	sed -i -e "/\/opt\/farm\/ext\/farm-inspector\/cron\/network.sh/d" /etc/crontab
-fi
+/opt/farm/scripts/setup/extension.sh sf-inspect-usage
+/opt/farm/scripts/setup/extension.sh sf-inspect-users
+/opt/farm/scripts/setup/extension.sh sf-inspect-pending
+/opt/farm/scripts/setup/extension.sh sf-inspect-routers
--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if grep -q /opt/farm/ext/farm-inspector/cron /etc/crontab; then
-	sed -i -e "/\/opt\/farm\/ext\/farm-inspector\/cron/d" /etc/crontab
-fi
